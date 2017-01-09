901c6dcf9cc5850b4a4b281a54365acb72084e4e
hankrum <st_kolev@mail.bg>
2017-01-09 09:48:27 +0200

    Added Add-remove-erased methods in Recipe

--- CookBook/Contracts/Recipe.cs
+++ CookBook/Contracts/Recipe.cs
@@ -12,6 +12,16 @@ namespace CookBook.Contracts
         IEnumerable<Product> Products { get; }
         IEnumerable<Preparation> Preparations { get; }
 
+        void AddProduct(Product product);
+        void RemoveProduct(Product product);
+        void RemoveProduct(string name);
+        void EraseAllProducts();
+
+        void AddPreparation(Preparation preparation);
+        void RemovePreparation(Preparation preparation);
+        void RemovePreparation(string name);
+        void EraseAllPreparations();
+
         RecipeType RecipeKind { get; }
     }
 }
