8692c33ae3eb17677e1fef426e270af0b867506b
hankrum <st_kolev@mail.bg>
2017-01-11 13:42:07 +0200

    Class Preparations and Builder

---
+++ CookBook/Data/Preparations.cs
@@ -0,0 +1,64 @@
+﻿using CookBook.Common;
+using CookBook.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CookBook.Data
+{
+    public class Preparations 
+    {
+        private List<Preparation> preparationList;
+
+        List<Preparation> PreparationList
+        {
+            get
+            {
+                return this.preparationList;
+            }
+        }
+
+        public Preparations()
+        {
+            this.preparationList = new List<Preparation>();
+        }
+
+        public void Add(Preparation item)
+        {
+            Validation.IsNull(item);
+            this.preparationList.Add(item);
+        }
+
+        public void EraseAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove<T>(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove<T>(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Preparations Builder(XmlNode item)
+        {
+            Preparations result = new Preparations();
+
+            foreach (XmlNode node in item.ChildNodes)
+            {
+                Preparation pItem = Preparation.Builder(node);
+                result.Add(pItem);
+            }
+
+            return result;
+        }
+
+    }
+}
